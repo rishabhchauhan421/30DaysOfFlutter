930f4e2496c001ee09e6689c7570d8a9d2db84ba
Rishabh Chauhan <rshbhchauhan@gmail.com>
2023-04-17 21:14:14 +0530

    Day 9 Completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String imageURL;
+
+  Item({
+    required this.id,
+    required this.name,
+    required this.desc,
+    required this.price,
+    required this.color,
+    required this.imageURL,
+  });
+}
